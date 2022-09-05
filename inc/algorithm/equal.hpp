532aad164c515b501670e3625f4c9ee03b883cba
Justine Saint-Jalmes <jusaint-@student.42.fr>
2022-09-05 18:40:58 +0200

    add: equal prototypes

---
+++ inc/algorithm/equal.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   equal.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusaint- <jusaint-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 17:27:14 by jusaint-          #+#    #+#             */
+/*   Updated: 2022/09/05 18:16:49 by jusaint-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EQUAL_HPP
+# define EQUAL_HPP
+
+# include "../utils.hpp"
+
+// returns true if the range [first1, last1) is equal to the range [first2, first2 + (last1 - first1)), 
+// and false otherwise.
+
+namespace ft {
+
+	template<class InputIt1, class InputIt2>
+	bool	equal(InputIt1 first1, InputIt1 last1, InputIt2 first2);
+	
+	template<class InputIt1, class InputIt2, class BinaryPredicate>
+	bool	equal(	InputIt1 first1, 
+					InputIt1 last1,	
+					InputIt2 first2,
+					BinaryPredicate p );
+}	/* namespace ft */
+
+#endif	/* EQUAL_HPP */
